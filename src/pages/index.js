ec3e0669c75546fbf381de8e2d3cb7f239e8d7d6
Karol Moroz <k.j.moroz@gmail.com>
2020-04-16 00:52:26 +0800

    Add some content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>
+      Dear friends, this is the website and blog of the YouTube Channel Make
+      Programming Fun Again.
+    </p>
+    <p>
+      The mission of my channel is to make programming fun again by promoting
+      the use of modern, fun-to-use, production-ready technologies, such as
+      Elixir, React, and GraphQL. Leave your Boring Stack&trade; at work and
+      enjoy programming with fun stuff!
+    </p>
+    <p>
+      In the following days, I will improve this website and post more
+      information about functional programming and Web development. I am also
+      planning to provide more content in Chinese. Until then, please enjoy the
+      videos below.
+    </p>
+    <p>Yours, Karol Moroz</p>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/MxGQrmqubsI"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/8MCmTiB3g6c"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </Layout>
 )
 
