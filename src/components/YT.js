a8a088ab01ac941aa00626496421c56a2bdb08b9
Karol Moroz <k.j.moroz@gmail.com>
2020-04-24 18:05:08 +0800

    Make YT embeds responsive
    https://flaviocopes.com/responsive-youtube-videos/

--- src/components/YT.js
+++ src/components/YT.js
@@ -2,16 +2,15 @@ import React from "react";
 
 const YT = ({ youtube, title }) => {
   return (
-    <iframe
-      width="560"
-      height="315"
-      className="responsive_yt"
-      title={title}
-      src={`https://www.youtube.com/embed/${youtube}`}
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    />
+    <div className="responsive_yt">
+      <iframe
+        title={title}
+        src={`https://www.youtube.com/embed/${youtube}`}
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+    </div>
   );
 };
 
